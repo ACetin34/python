e7bb7c6eee5312cc96aece6f0027d52543040d11
ACetin34 <36761452+ACetin34@users.noreply.github.com>
2018-03-02 13:16:44 +0300

    Update HW1.py

--- Lecture1/HW1.py
+++ Lecture1/HW1.py
@@ -1,4 +1,5 @@
-for i in range (0,3) :
+f = int(input("İşlem Kaç Defa Tekrar Etsin"))
+for i in range (0,f) :
     a = int(input("1. Sayıyı Girin :"))
     b = int(input("2. Sayıyı Girin :"))
     islem = input("İşlem Operatörü Seçiniz (+,-,*,/):")
@@ -13,8 +14,11 @@ for i in range (0,3) :
     elif islem == "*" :
         print (a*b)
 
+    elif b==0 :
+        print ("Sıfır ile bölme işlemi yapamazsınız.")
     elif islem =="/" :
         print (a/b)
+
     elif islem == "exit":
         break
     else:
