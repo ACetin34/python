46156f066f15ae210992536dfb523308f8778524
ACetin34 <arda.cetin34@gmail.com>
2018-03-15 17:09:43 +0300

    3.Hafta Ödevler

---
+++ Week3/HW4.py
@@ -0,0 +1,13 @@
+def reverse(str):
+  x=len(str)
+  arr=[]
+  for i in range(x):
+    arr.append(str[i])
+  ort=x/2
+  ort=int(ort)
+  for i in range (0,ort):
+    arr[i], arr[0 - (i + 1)] = arr[0 - (i + 1)] , arr[i]
+  arr="".join(arr)
+  return arr
+kelime=input("Bir kelime giriniz : ")
+print(reverse(kelime))
